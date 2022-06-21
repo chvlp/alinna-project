d77eab9ae62fbc91379448f3d92b3fa86e80cdcd
chvlp <49900011+chvlp@users.noreply.github.com>
2022-06-21 17:25:23 +0700

    main

--- resources/views/admin/rentRoom/show.blade.php
+++ resources/views/admin/rentRoom/show.blade.php
@@ -31,8 +31,9 @@
                   <div class="col-lg-8">
                     <div class="row">
 
-                      <div class="col-xl-6 mb-xl-0 mb-4" >
-                        <div class="card bg-transparent shadow-xl">
+                      <div  class="col-xl-6 mb-xl-0 mb-4" >
+
+                        <div style="border:1px solid red" class="card bg-transparent shadow-xl">
                           <div class="overflow-hidden position-relative border-radius-xl">
                             <img src="{{ asset('storage/images/rooms/'.$rentRooms->room->image) }}" class="position-absolute opacity-2 start-0 top-0 w-100 z-index-1 h-100" alt="pattern-tree">
                             <span class="mask bg-gradient-dark opacity-10"></span>
@@ -58,6 +59,7 @@
                             </div>
                           </div>
                         </div>
+
                       </div>
           
                       <div class="col-xl-6 mb-xl-0 mb-4" style="font-family: 'Noto sans Lao">
@@ -96,56 +98,38 @@
                               </div>
                             </div>
                           </div>
-
                           <div  class="card-body p-3" >
                             <div class="row">
-
-                             
                                 <div  class="col-xl-6">
-                                  <div  class="row">
-                                      @foreach ($rentRoomStory as $item)
-                                        <div class="col-md-6 col-6" >
-                                          <div class="card">
-                                            <div class="card-header mx-4 p-3 text-center" >
-                                              <div class="border-radius-lg">
-                                                <img class="border-radius-lg" src="{{ asset('storage/images/profiles/'.$item->rentRoom->member->user->image) }}" alt="" width="100%">
+                                      <div class="col-lg-6 col-md-6 col-sm-6 mb-md-6 mb-6">
+                                        <div class="card">
+                                          @foreach ($rentRoomStory as $item)
+
+                                            <div style="border:1px solid red" class="card bg-transparent shadow-xl">
+                                              <div class="overflow-hidden position-relative border-radius-xl">
+                                                <span class="mask bg-gradient-dark opacity-10"></span>
+                                                <div class="card-body position-relative z-index-1 p-3" style="font-family: 'Noto sans Lao">
+                                                  <h5 class="text-white  mt-4 mb-5 pb-2">ເບີຫ້ອງ: {{ $rentRooms->room->number }}</h5>
+                                                  <div class="d-flex">
+                                                    <div class="d-flex">
+                                                      <div>
+                                                        <p class="text-white text-sm opacity-8 mb-0">ວ.ດ.ປ ເຊົ່າ</p>
+                                                        <h6 class="text-white mb-0">{{ date('d-m-Y'),strtotime($item->rentRoom->intodate) }}</h6>
+                                                      </div>
+                                                    </div>
+                                                    <div class="ms-auto walign-items-end justify-content-end">
+                                                      <p class="text-white text-sm opacity-8 mb-0">ວັນໝົດອາຍຸ</p>
+                                                      <h6 class="text-white mb-0">{{ date('d-m-Y'),strtotime($item->rentRoom->outdate) }}</h6>
+                                                    </div>
+                                                  </div>
+                                                </div>
                                               </div>
                                             </div>
-                                            <div class="card-body pt-0 p-3 text-center" style="font-family: 'Noto sans Lao">
-                                              <p class="mb-0 text-dark font-weight-bold text-sm"> 
-                                                <span class="text-xs">ວ.ດ.ປ ເຊົ່າ:</span> {{ date('d-m-y'),strtotime($item->rentRoom->intodate) }}
-                                              </p>
-                                              <p class="mb-0 text-dark font-weight-bold text-sm"> 
-                                                <span class="text-xs">ໝົດກຳນົດ:</span> {{ date('d-m-y'),strtotime($item->rentRoom->outdate) }}
-                                              </p>
-                                              <h5 class="mb-0"><span class="text-xs">ລາຄາ:</span>{{ $item->rentRoom->room->price }} ₭</h5>
-                                            </div>
-                                          </div>
+
+                                          @endforeach
                                         </div>
-                                    @endforeach
-
-
-                                      <div class="col-md-6 col-6">
-                                        <div class="card">
-                                          <div class="card-header mx-4 p-3 text-center">
-                                            <div class="icon icon-shape icon-lg bg-gradient-primary shadow text-center border-radius-lg">
-                                              <i class="material-icons opacity-10">account_balance</i>
-                                            </div>
-                                          </div>
-                                          <div class="card-body pt-0 p-3 text-center">
-                                            <h6 class="text-center mb-0">Salary</h6>
-                                            <span class="text-xs">Belong Interactive</span>
-                                            <hr class="horizontal dark my-3">
-                                            <h5 class="mb-0">+$2000</h5>
-                                          </div>
-                                        </div>
-                                      </div>
-
-                                  </div>
+                                    </div>
                                 </div>
-
-                                
-
                             </div>
                           </div>
                         </div>
@@ -225,6 +209,171 @@
     </div>
   </div>
 
+  <div class="row mb-4">
+    <div class="col-lg-6 col-md-6 col-sm-0 mb-md-3 mb-3">
+      <div class="card my-4">
+        <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+          <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
+
+              <div class="bg-gradient-primary">
+                <div class="row">
+                  <div class="col-lg-6 col-7">
+                    <h6 style="" class="text-white text-capitalize ps-3">ຂໍ້ມູນກນຊຳລະຄ່າໄຟຟ້າ</h6>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+        <div class="card-body px-0 pb-2">
+          <div class="table-responsive p-0">
+            <div class="container-fluid py-4" style="font-family: 'Noto sans Lao">
+              <div class="row">
+                <div class="card h-100">
+                  <div class="card-body p-3 pb-0">
+                    <ul class="list-group">
+                      <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+                        <div class="d-flex flex-column">
+                          <h6 class="mb-1 text-dark font-weight-bold text-sm">March, 01, 2020</h6>
+                          <span class="text-xs">#MS-415646</span>
+                        </div>
+                        <div class="d-flex align-items-center text-sm">
+                          $180
+                          <button class="btn btn-link text-dark text-sm mb-0 px-0 ms-4"><i class="material-icons text-lg position-relative me-1">picture_as_pdf</i> PDF</button>
+                        </div>
+                      </li>
+                      <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+                        <div class="d-flex flex-column">
+                          <h6 class="text-dark mb-1 font-weight-bold text-sm">February, 10, 2021</h6>
+                          <span class="text-xs">#RV-126749</span>
+                        </div>
+                        <div class="d-flex align-items-center text-sm">
+                          $250
+                          <button class="btn btn-link text-dark text-sm mb-0 px-0 ms-4"><i class="material-icons text-lg position-relative me-1">picture_as_pdf</i> PDF</button>
+                        </div>
+                      </li>
+                      <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+                        <div class="d-flex flex-column">
+                          <h6 class="text-dark mb-1 font-weight-bold text-sm">April, 05, 2020</h6>
+                          <span class="text-xs">#FB-212562</span>
+                        </div>
+                        <div class="d-flex align-items-center text-sm">
+                          $560
+                          <button class="btn btn-link text-dark text-sm mb-0 px-0 ms-4"><i class="material-icons text-lg position-relative me-1">picture_as_pdf</i> PDF</button>
+                        </div>
+                      </li>
+                      <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+                        <div class="d-flex flex-column">
+                          <h6 class="text-dark mb-1 font-weight-bold text-sm">June, 25, 2019</h6>
+                          <span class="text-xs">#QW-103578</span>
+                        </div>
+                        <div class="d-flex align-items-center text-sm">
+                          $120
+                          <button class="btn btn-link text-dark text-sm mb-0 px-0 ms-4"><i class="material-icons text-lg position-relative me-1">picture_as_pdf</i> PDF</button>
+                        </div>
+                      </li>
+                      <li class="list-group-item border-0 d-flex justify-content-between ps-0 border-radius-lg">
+                        <div class="d-flex flex-column">
+                          <h6 class="text-dark mb-1 font-weight-bold text-sm">March, 01, 2019</h6>
+                          <span class="text-xs">#AR-803481</span>
+                        </div>
+                        <div class="d-flex align-items-center text-sm">
+                          $300
+                          <button class="btn btn-link text-dark text-sm mb-0 px-0 ms-4"><i class="material-icons text-lg position-relative me-1">picture_as_pdf</i> PDF</button>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-lg-6 col-md-6 col-sm-0 mb-md-3 mb-3">
+    <div class="card my-4">
+      <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+        <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
+
+            <div class="bg-gradient-primary">
+              <div class="row">
+                <div class="col-lg-6 col-7">
+                  <h6 style="" class="text-white text-capitalize ps-3">ຂໍ້ມູນກນຊຳລະຄ່ານ້ຳປະປາ</h6>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+      <div class="card-body px-0 pb-2">
+        <div class="table-responsive p-0">
+          <div class="container-fluid py-4" style="font-family: 'Noto sans Lao">
+            <div class="row">
+              <div class="card h-100">
+                <div class="card-body p-3 pb-0">
+                  <ul class="list-group">
+                    <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+                      <div class="d-flex flex-column">
+                        <h6 class="mb-1 text-dark font-weight-bold text-sm">March, 01, 2020</h6>
+                        <span class="text-xs">#MS-415646</span>
+                      </div>
+                      <div class="d-flex align-items-center text-sm">
+                        $180
+                        <button class="btn btn-link text-dark text-sm mb-0 px-0 ms-4"><i class="material-icons text-lg position-relative me-1">picture_as_pdf</i> PDF</button>
+                      </div>
+                    </li>
+                    <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+                      <div class="d-flex flex-column">
+                        <h6 class="text-dark mb-1 font-weight-bold text-sm">February, 10, 2021</h6>
+                        <span class="text-xs">#RV-126749</span>
+                      </div>
+                      <div class="d-flex align-items-center text-sm">
+                        $250
+                        <button class="btn btn-link text-dark text-sm mb-0 px-0 ms-4"><i class="material-icons text-lg position-relative me-1">picture_as_pdf</i> PDF</button>
+                      </div>
+                    </li>
+                    <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+                      <div class="d-flex flex-column">
+                        <h6 class="text-dark mb-1 font-weight-bold text-sm">April, 05, 2020</h6>
+                        <span class="text-xs">#FB-212562</span>
+                      </div>
+                      <div class="d-flex align-items-center text-sm">
+                        $560
+                        <button class="btn btn-link text-dark text-sm mb-0 px-0 ms-4"><i class="material-icons text-lg position-relative me-1">picture_as_pdf</i> PDF</button>
+                      </div>
+                    </li>
+                    <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+                      <div class="d-flex flex-column">
+                        <h6 class="text-dark mb-1 font-weight-bold text-sm">June, 25, 2019</h6>
+                        <span class="text-xs">#QW-103578</span>
+                      </div>
+                      <div class="d-flex align-items-center text-sm">
+                        $120
+                        <button class="btn btn-link text-dark text-sm mb-0 px-0 ms-4"><i class="material-icons text-lg position-relative me-1">picture_as_pdf</i> PDF</button>
+                      </div>
+                    </li>
+                    <li class="list-group-item border-0 d-flex justify-content-between ps-0 border-radius-lg">
+                      <div class="d-flex flex-column">
+                        <h6 class="text-dark mb-1 font-weight-bold text-sm">March, 01, 2019</h6>
+                        <span class="text-xs">#AR-803481</span>
+                      </div>
+                      <div class="d-flex align-items-center text-sm">
+                        $300
+                        <button class="btn btn-link text-dark text-sm mb-0 px-0 ms-4"><i class="material-icons text-lg position-relative me-1">picture_as_pdf</i> PDF</button>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+</div>
   @if ($message = Session::get('success'))
   <span style="color: #0fa034"><i class="fas fa-check"></i> </span>
   <div class="position-fixed bottom-1 end-1 z-index-2">
